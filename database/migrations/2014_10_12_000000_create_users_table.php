286b72d487af1e7d8cf41d83cc3893a0f1099789
ekharisma <ekharisma@outlook.com>
2021-07-17 08:39:40 +0700

    change migration:user file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,10 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->integer("kementerian_id");
+            $table->rememberToken();
             $table->timestamps();
         });
     }
